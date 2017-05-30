ffe00b066ed759b4a1c5780222d2f3d58518e035
batuto <mariano@vauxoo.com>
2017-05-30 17:54:35 -0500

    [ADD] Added initial base of proyect

--- almacen/models/product.py
+++ almacen/models/product.py
@@ -5,8 +5,8 @@ from openerp import models, fields
 class Product(models.Model):
 	_name = 'product'
 
-	kind = fields.Selection([(0,'Comestibles'), (1,'Jardineria'),
-							(2,'Farmacia'), (3,'Ferreteria')],
+	kind = fields.Selection([(1,'Comestibles'), (2,'Jardineria'),
+							(3,'Farmacia'), (4,'Ferreteria')],
 							string='Tipo de producto')
 	name = fields.Char(string = "Nombre")
 	price = fields.Float(string = "Precio")
